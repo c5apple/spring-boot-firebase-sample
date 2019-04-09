86df7defbcab86aca00eb327043b1145ee10f24b
c5apple <c5apple_party@yahoo.co.jp>
2019-04-10 01:37:04 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SpringBootFirebaseSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirebaseSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirebaseSampleApplication.class, args);
+	}
+
+}
